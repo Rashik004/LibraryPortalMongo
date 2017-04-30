9d3497008e2c6e262f98ce23a300641938535e2c
Rashik Hasnat <rashikhasnat@ymail.com>
2017-05-01 04:42:22 +0600

    Added ObjectIdConverter for JSON Converson of ObjectId

--- Common/Models/CrossOver.DataLayer.Models/Models/Book.cs
+++ Common/Models/CrossOver.DataLayer.Models/Models/Book.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MongoDB.Bson;
+using Newtonsoft.Json;
+using System.Web.UI;
+using CrossOver.DataLayer.Models;
 
 namespace CrossOver.DataAccessLayer.DBModel
 {
     public class Book
     {
+        [JsonConverter(typeof(ObjectIdConverter))]
         public ObjectId Id { get; set; }
         public string Title { get; set; }
         public string Publisher { get; set; }
