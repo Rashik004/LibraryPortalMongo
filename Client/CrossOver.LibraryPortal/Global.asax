443414fa312f639863c4502846309c38881cd5b3
Rashik Hasnat <rashikhasnat@ymail.com>
2017-05-01 00:22:26 +0600

    Added Library Portal.

---
+++ Client/CrossOver.LibraryPortal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrossOver.LibraryPortal.MvcApplication" Language="C#" %>
---
+++ Client/CrossOver.LibraryPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrossOver.LibraryPortal.Startup))]
+namespace CrossOver.LibraryPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
