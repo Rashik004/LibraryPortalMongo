bba6935ff22409a74a807cff0bfb46f25369dc79
Rashik Hasnat <rashikhasnat@ymail.com>
2017-05-01 04:43:23 +0600

    Changes for auth in portal

---
+++ Server/CrossOver.BusinessLayer.BookDataRepository/CrossOver.BusinessLayer.Repositories/Interfaces/IAuthenticationRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrossOver.DataAccessLayer.DBModel;
+
+namespace CrossOver.BusinessLayer.Repositories.Interfaces
+{
+    public interface IAuthenticationRepository
+    {
+        User GetUserDetails(User user);
+    }
+}
---
+++ Server/CrossOver.BusinessLayer.BookDataRepository/CrossOver.BusinessLayer.Repositories/Repository/AuthenticationRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrossOver.BusinessLayer.Repositories.Interfaces;
+using CrossOver.DataAccessLayer.DBModel;
+using MongoDB.Driver;
+
+namespace CrossOver.BusinessLayer.Repositories.Repository
+{
+    public class AuthenticationRepository:IAuthenticationRepository
+    {
+        private readonly DBUnitOfWork _db;
+
+        public AuthenticationRepository()
+        {
+            _db=new DBUnitOfWork();
+        }
+
+        public User GetUserDetails(User user)
+        {
+            var authenticatedUser = _db.Users
+                .Find(u => u.Username == user.Username && u.Password == user.Password)
+                .FirstOrDefaultAsync().Result;
+            return authenticatedUser;
+        }
+    }
+}
